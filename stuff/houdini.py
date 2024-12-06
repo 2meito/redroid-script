7ec3ae2e1a7950733bad4351525817a8cd36ebe8
2meito <22meito@gmail.com>
2024-12-05 19:34:13 -0500

    Added houdini arguments

--- stuff/houdini.py
+++ stuff/houdini.py
@@ -3,6 +3,7 @@ import re
 import shutil
 from stuff.general import General
 from tools.helper import bcolors, get_download_dir, print_color, run
+from tools.scripton import patch_libhoudini
 
 
 class Houdini(General):
@@ -79,7 +80,7 @@ on property:sys.boot_completed=1
         run(["chmod", "+x", self.extract_to, "-R"])
 
         print_color("Copying libhoudini library files ...", bcolors.GREEN)
-        name = re.findall("([a-zA-Z0-9]+)\.zip", self.dl_link)[0]
+        name = re.findall(r"([a-zA-Z0-9]+)\.zip", self.dl_link)[0]
         shutil.copytree(os.path.join(self.extract_to, "vendor_intel_proprietary_houdini-" + name,
                         "prebuilts"), os.path.join(self.copy_dir, "system"), dirs_exist_ok=True)
 
@@ -89,3 +90,4 @@ on property:sys.boot_completed=1
         with open(init_path, "w") as initfile:
             initfile.write(self.init_rc_component)
         os.chmod(init_path, 0o644)
+        patch_libhoudini(os.path.join(self.copy_dir, "system", "lib64", "libhoudini.so", ))
